57553486c30703d06db76a9d5651d6b6f436b849
Jared Parsons <jaredpar@microsoft.com>
2016-07-06 12:39:15 -0700

    Move most of our NuGet package building out of Setup.proj

--- src/Tools/MicroBuild/PostBuild.ps1
+++ src/Tools/MicroBuild/PostBuild.ps1
@@ -2,16 +2,24 @@
 .SYNOPSIS
 Performs any post-build actions needed on the Roslyn build systems.
 
-.PARAMETER BinariesDirectory
+.PARAMETER binariesPath
 The root directory where the build outputs are written: e:\path\to\source\Binaries\Debug
 
-.PARAMETER SourceDirectory
+.PARAMETER sourcePath
 The root directory where the sources are checked out: e:\path\to\source
+
+.PARAMETER platform
+The platform being built
+
+.PARAMETER configuration
+The configuration being built
 
 #>
 param(
-    [string]$binariesDirectory,
-    [string]$sourceDirectory,
+    [string]$binariesPath,
+    [string]$sourcePath,
+    [string]$configuration,
+    [string]$platform
 )
 
 set-strictmode -version 2.0
@@ -20,6 +28,15 @@ $ErrorActionPreference="Stop"
 try
 {
     write-host "Post Build Steps"
+
+    write-host "Building the NuGets"
+    $msbuildExe = & (join-path $sourcePath "build\scripts\get-msbuildpath.ps1")
+    $nugetProj = join-path $sourcePath "src\Nuget\NuGet.proj"
+
+    & $msbuildExe /v:m /nodereuse:false /p:Platform=$platform /p:Configuration=$configuration $nugetProj
+    if ($lastExitCode -ne 0) {
+        throw "Build failed"
+    }
 
     exit 0
 }
